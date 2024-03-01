5759107aa97c8ec8dc151dc46e6d74fc74bc9e40
yashtealvue <yashvendra.kumar@tealvue.com>
2024-03-01 16:54:03 +0530

    aygsda

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import Pagination from "./Pagination";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Pagination />
+    </div>
+  );
+}
---
+++ src/Pagination.js
@@ -0,0 +1,78 @@
+import React, { useState, useEffect } from "react";
+
+const Pagination = () => {
+  const [data, setData] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 10;
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch(
+        "https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json"
+      );
+      const result = await response.json();
+      setData(result);
+    } catch (error) {
+      alert("Failed to fetch data");
+    }
+  };
+
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentItems = data.slice(indexOfFirstItem, indexOfLastItem);
+
+  const handleNextPage = () => {
+    if (currentPage < Math.ceil(data.length / itemsPerPage)) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+
+  const handlePreviousPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
+
+  return (
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>role</th>
+          </tr>
+        </thead>
+        <tbody>
+          {currentItems.map((item) => (
+            <tr key={item.id}>
+              <td>{item.id}</td>
+              <td>{item.name}</td>
+              <td>{item.email}</td>
+              <td>{item.role}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <div>
+        <button onClick={handlePreviousPage} >
+          Previous
+        </button>
+        <span> {currentPage} </span>
+        <button
+          onClick={handleNextPage}
+          disabled={currentPage === Math.ceil(data.length / itemsPerPage)}
+        >
+          Next
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Pagination;
